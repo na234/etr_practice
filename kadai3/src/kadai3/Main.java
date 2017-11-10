7434a3f835bca0292ac004b56ad08c834d2e0f90
na234 <s17t281@stu.kagawa-u.ac.jp>
2017-11-10 20:49:42 +0900

    Lesson1 問3

---
+++ kadai3/src/kadai3/Main.java
@@ -0,0 +1,17 @@
+package kadai3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int a = 6;
+		int b;
+		
+		for(b = 1; b <= a; b++) {
+			System.out.printf("%d ", b);
+		}
+
+	}
+
+}
