6d0a4d187020a806bc08ce133c68f1454596f353
na234 <s17t281@stu.kagawa-u.ac.jp>
2017-11-24 21:18:30 +0900

    クイズ
    問題文は、まだ作ってないです

---
+++ Quiz/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Question q = new Question();
+		
+		q.n01();
+
+	}
+	
+
+}
---
+++ Quiz/src/Question.java
@@ -0,0 +1,12 @@
+
+public class Question {
+	void n01() {
+		System.out.println();
+		int a = new java.util.Scanner(System.in).nextInt();
+		
+		if(a == 1) {
+			System.out.println("正解です");
+		}
+	}
+
+}
